e88a6a448b3446a13bbdb95975a4bfeff8dc84c4
Anjana Kavidu <anjanakavid@gmail.com>
2024-02-29 16:51:12 +0530

    Update

--- UniversityMIS/src/main/java/mis/university/Main.java
+++ UniversityMIS/src/main/java/mis/university/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("profile.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
         primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
