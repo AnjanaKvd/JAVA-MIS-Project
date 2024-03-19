33b9f3ee7123d01e07848184e7b36fe9b312496e
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-03-19 23:24:58 +0530

    UplodMedical

--- UniversityMIS/src/main/java/mis/university/TecDashbord.java
+++ UniversityMIS/src/main/java/mis/university/TecDashbord.java
@@ -1,4 +1,27 @@
 package mis.university;
 
+import javafx.fxml.FXML;
+
 public class TecDashbord {
+    @FXML
+    private  void Attendance(){
+        System.out.println("mali");
+
+    }
+    @FXML
+    private void Notice(){
+
+    }
+    @FXML
+    private void profile(){
+
+    }
+    @FXML
+    private void Medical(){
+
+    }
+    @FXML
+    private void timetable(){
+
+    }
 }
