9967cae2a17e6fdc4fab7f37bb545fcb9a6a88e9
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-03-20 02:14:31 +0530

    tecoffuserprofile

---
+++ UniversityMIS/src/main/java/mis/university/TecOffUserProfile.java
@@ -0,0 +1,10 @@
+package mis.university;
+
+import javafx.fxml.FXML;
+
+public class TecOffUserProfile {
+    @FXML
+    private void updateprofile(){
+        System.out.println("mali");
+    }
+}
---
+++ UniversityMIS/src/main/java/mis/university/UpdateTecprofile.java
@@ -0,0 +1,10 @@
+package mis.university;
+
+import javafx.fxml.FXML;
+
+public class UpdateTecprofile {
+    @FXML
+    private void update(){
+        System.out.println("mali");
+    }
+}
