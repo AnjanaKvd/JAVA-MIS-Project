7dfc4d20d03514b8deea060f17411ca0c61e7f64
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-03-20 15:08:37 +0530

    end

--- UniversityMIS/src/main/java/mis/university/TecDashbord.java
+++ UniversityMIS/src/main/java/mis/university/TecDashbord.java
@@ -1,9 +1,15 @@
 package mis.university;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TecDashbord {
     @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     private  void Attendance(){
         System.out.println("mali");
 
