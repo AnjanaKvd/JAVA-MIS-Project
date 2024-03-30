ed39e970e655677c9ae567c8f165f56a0298c75f
Anjana Kavidu <anjanakavid@gmail.com>
2024-03-30 15:43:50 +0530

    Create new version

--- UniversityMISv2/src/main/java/module-info.java
+++ UniversityMISv2/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module university.misv2.universitymisv2 {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
 
     opens university.misv2.universitymisv2 to javafx.fxml;
     exports university.misv2.universitymisv2;
