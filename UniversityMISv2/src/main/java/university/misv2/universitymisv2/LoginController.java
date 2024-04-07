c4de516131ff40b117423c301310614ae6c297f3
Anjana Kavidu <anjanakavid@gmail.com>
2024-04-07 13:59:52 +0530

    Update

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/LoginController.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/LoginController.java
@@ -1,5 +1,37 @@
 package university.misv2.universitymisv2;
 
+import javafx.animation.RotateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
 
+    @FXML
+    private ImageView closeBtn;
+
+    @FXML
+    private void initialize() {
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.4), closeBtn);
+        rotateTransition.setByAngle(360);
+        rotateTransition.setAutoReverse(false);
+        rotateTransition.setCycleCount(1);
+        closeBtn.setOnMouseEntered(event -> rotateTransition.play());
+        closeBtn.setOnMouseExited(event -> rotateTransition.stop());
+        closeBtn.setOnMouseExited(event -> rotateTransition.setFromAngle(0));
+    }
+
+    @FXML
+    private void handleCloseButtonAction() {
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
+    }
 }
