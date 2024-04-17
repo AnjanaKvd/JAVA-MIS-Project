6b3ff514b7d1bb23af9858e9401d9ed6fd5e77c0
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-04-17 13:53:39 +0530

    start

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TechnicalOfficerController.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TechnicalOfficerController.java
@@ -1,4 +1,71 @@
 package university.misv2.universitymisv2.technicalOfficer;
 
+import javafx.animation.RotateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 public class TechnicalOfficerController {
+
+    @FXML
+    private Label profileNameLabel;
+
+    @FXML
+    private Label profileRoleLabel;
+
+    @FXML
+    private ImageView closeBtn;
+
+    @FXML
+    private HBox dashboardOption1;
+
+    @FXML
+    private HBox dashboardOption2;
+
+    @FXML
+    private HBox dashboardOption3;
+
+    @FXML
+    private HBox dashboardOption4;
+
+    @FXML
+    private HBox dashboardOption5;
+
+    @FXML
+    private void initialize() {
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.4), closeBtn);
+        rotateTransition.setByAngle(360);
+        rotateTransition.setAutoReverse(false);
+        rotateTransition.setCycleCount(1);
+        closeBtn.setOnMouseEntered(event -> rotateTransition.play());
+        closeBtn.setOnMouseExited(event -> rotateTransition.stop());
+        closeBtn.setOnMouseExited(event -> rotateTransition.setFromAngle(0));
+
+        // Add event handlers for side options
+        dashboardOption1.setOnMouseClicked(event -> handleDashboardOptionSelected(dashboardOption1));
+        dashboardOption2.setOnMouseClicked(event -> handleDashboardOptionSelected(dashboardOption2));
+        dashboardOption3.setOnMouseClicked(event -> handleDashboardOptionSelected(dashboardOption3));
+        dashboardOption4.setOnMouseClicked(event -> handleDashboardOptionSelected(dashboardOption4));
+        dashboardOption5.setOnMouseClicked(event -> handleDashboardOptionSelected(dashboardOption5));
+
+//         Set the default selected option
+        handleDashboardOptionSelected(dashboardOption1);
+    }
+
+    @FXML
+    private void handleCloseButtonAction() {
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    private void handleDashboardOptionSelected(HBox selectedOption) {
+        VBox dashboardOptionsContainer = (VBox) selectedOption.getParent();
+        dashboardOptionsContainer.getChildren().forEach(option -> option.getStyleClass().setAll("dashboard-option"));
+        selectedOption.getStyleClass().setAll("dashboard-option-selected");
+    }
 }
