f0753b22b707f38268b9adb7590387abcc9b55cb
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-04-20 10:26:05 +0530

    2024/04/20

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/Medical.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/Medical.java
@@ -1,5 +1,27 @@
 package university.misv2.universitymisv2.technicalOfficer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Medical {
-    public static String fxml;
+    @FXML
+    private Stage stage ;
+    private Scene scene;
+    private Parent root;
+
+    public  void gotecdash(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("tec dash.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TecDash.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TecDash.java
@@ -15,8 +15,12 @@ public class TecDash {
     private Stage stage ;
     private Scene scene;
     private Parent root;
-    public  void ButtonClick(){
-        System.out.println("Button click..........");
+    public  void goAttendance(ActionEvent event)throws IOException{
+        Parent rot = FXMLLoader.load(getClass().getResource("Attendance.fxml"));
+        stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene =new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     public  void goMedical(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Medical.fxml"));
