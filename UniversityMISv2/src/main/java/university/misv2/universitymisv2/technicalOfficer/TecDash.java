885855fb45597738aeab6178b2494b8b1adfb0b1
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-04-25 00:30:01 +0530

    98

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TecDash.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TecDash.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -32,11 +33,27 @@ public class TecDash {
         stage.show();
     }
 
-    public  void GoAttendance(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Attendance.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+//    @FXML
+//    public  void go(ActionEvent event) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("Attendance.fxml"));
+//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+
+    @FXML
+    public void go(ActionEvent event) throws IOException {
+        AnchorPane root = new AnchorPane(); // Create a new AnchorPane as the root element
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Attendance.fxml"));
+        loader.setRoot(root); // Set the root element
+        Parent parent = loader.load();
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
     }
+
 }
