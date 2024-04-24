e77d75bcd297096cb51f5a596a4ab8959f9db3e1
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-04-24 14:55:08 +0530

    98

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/Main.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
