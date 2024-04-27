c684d778a09e4076e2b070dba529ab26b38bc745
mpwijesiri <maheshika_20211083@fot.ruh.ac.lk>
2024-04-27 22:35:00 +0530

    mk

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TechnicalOfficerController.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/technicalOfficer/TechnicalOfficerController.java
@@ -3,6 +3,7 @@ package university.misv2.universitymisv2.technicalOfficer;
 import javafx.animation.RotateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -48,6 +49,56 @@ public class TechnicalOfficerController {
 
     @FXML
     private HBox dashboardOption5;
+
+    @FXML
+    private AnchorPane Dashboard;
+
+    @FXML
+    private AnchorPane Attendance;
+
+    @FXML
+    private AnchorPane Medical;
+
+    @FXML
+    public AnchorPane Timetable;
+
+    @FXML
+    private AnchorPane Notices;
+
+
+    @FXML
+    private TextField Student_idField;
+
+    @FXML
+    private TextField courseNameField;
+
+    @FXML
+    private TextField theory_or_practicalField;
+
+    @FXML
+    private TextField dateFiled;
+
+    @FXML
+    private TextField StatusField;
+
+    public TextField modifyStudentField;
+    public TextField newCourseNameField ;
+    public  TextField newtheory_or_practicalfiled;
+    public  TextField newdatamodifyField;
+    public  TextField newStatusField;
+
+
+    //medical
+    public  TextField addmedicalFiled;
+    public  TextField addStudent_idField;
+    public  TextField addcoursecodeField;
+    public  TextField addtheory_or_practicalFiled;
+    public  TextField adddateFiled;
+    public  TextField addStatusFiled;
+
+
+
+
 
     @FXML
     private void initialize() {
@@ -106,18 +157,18 @@ public class TechnicalOfficerController {
     public void AddAttendancesButton(ActionEvent event) {
         String Student_id =Student_idField.getText();
         String Course_code = courseNameField.getText();
-        String theory_or_practical = departmentDropdown.getValue();
-        Date  date=kj.getValue();
-        String Status = lecturerField.getText();
+        String theory_or_practical =theory_or_practicalField .getText();
+        Insets date=dateFiled.getOpaqueInsets();
+        String Status = StatusField.getText();
         try {
 
             CourseManager.AddAttendances(Student_id, Course_code, theory_or_practical, date, Status);
-            courseCodeField.clear();
+            Student_idField.clear();
             courseNameField.clear();
-            departmentDropdown.getSelectionModel().clearSelection();
-            creditsField.clear();
-            hoursField.clear();
-            lecturerField.clear();
+            theory_or_practicalField.clear();
+            dateFiled.clear();
+            StatusField.clear();
+            //lecturerField.clear();
             courseAddedLabel.setText("Attendance added successfully !");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -127,19 +178,19 @@ public class TechnicalOfficerController {
     }
     @FXML
     private void modifyAttendancesButten(ActionEvent event) {
-        String newStudent_id = modifyCourseCodeField.getText();
+        String newStudent_id = modifyStudentField.getText();
         String newCourse_code = newCourseNameField.getText();
-        String newtheory_or_practical = newDepartmentDropdown.getValue();
-        Date  newdate=kj.getValue();
-        String newStatus = newLecturerField.getText();
+        String newtheory_or_practical = newtheory_or_practicalfiled.getText();
+        Date  newdate=newdatamodifyField.getValue();
+        String newStatus = newStatusField.getText();
         try {
             CourseManager.modifyAttendances(newStudent_id, newCourse_code, newtheory_or_practical, newdate, newStatus);
-            modifyCourseCodeField.clear();
+            modifyStudentField.clear();
             newCourseNameField.clear();
-            newDepartmentDropdown.getSelectionModel().clearSelection();
-            newCreditsField.clear();
-            newHoursField.clear();
-            newLecturerField.clear();
+            //newDepartmentDropdown.getSelectionModel().clearSelection();
+            newtheory_or_practicalfiled.clear();
+            newdatamodifyField.clear();
+            newStatusField.clear();
             courseModifiedLabel.setText("Attendance modified successfully !");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -158,21 +209,22 @@ public class TechnicalOfficerController {
     }
 
     public void AddSubmitButton(ActionEvent event) {
-        String Medical_Id =lk.getText();
-        String Student_id =Student_idField.getText();
-        String Course_code = courseNameField.getText();
-        String theory_or_practical = departmentDropdown.getValue();
-        Date  date=kj.getValue();
-        String Status = lecturerField.getText();
+        String Medical_Id =addmedicalFiled.getText();
+        String Student_id =addStudent_idField.getText();
+        String Course_code = addcoursecodeField.getText();
+        String theory_or_practical = addtheory_or_practicalFiled.getText();
+        Date  date=adddateFiled.getValue();
+        String Status = addStatusFiled.getText();
         try {
 
             CourseManager.AddAttendances(Medical_Id,Student_id, Course_code, theory_or_practical, date, Status);
-            courseCodeField.clear();
+            addmedicalFiled.clear();
             courseNameField.clear();
-            departmentDropdown.getSelectionModel().clearSelection();
-            creditsField.clear();
-            hoursField.clear();
-            lecturerField.clear();
+            addcoursecodeField.clear();
+            addtheory_or_practicalFiled.clear();
+            adddateFiled.clear();
+            addStatusFiled.clear();
+
             courseAddedLabel.setText("Course Medical successfully !");
         } catch (SQLException e) {
             e.printStackTrace();
