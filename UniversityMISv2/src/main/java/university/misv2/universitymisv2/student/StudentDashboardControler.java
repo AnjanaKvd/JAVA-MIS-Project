2aa55a97799fc3326e3f9708aba5389470f348db
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2024-04-30 11:00:33 +0530

    bugs

--- UniversityMISv2/src/main/java/university/misv2/universitymisv2/student/StudentDashboardControler.java
+++ UniversityMISv2/src/main/java/university/misv2/universitymisv2/student/StudentDashboardControler.java
@@ -6,13 +6,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -20,8 +16,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-
-import static javafx.scene.paint.Color.SEAGREEN;
 
 public class StudentDashboardControler implements Initializable {
 
@@ -47,9 +41,9 @@ public class StudentDashboardControler implements Initializable {
     @FXML
     private ImageView studentsimage;
 
-    @FXML
-    private Circle user1;
 
+    @FXML
+    private ImageView user1;
 
 
     @Override
@@ -60,10 +54,9 @@ public class StudentDashboardControler implements Initializable {
         File college=new File("C:\\Users\\iTEC\\Desktop\\Java\\JAVA-MIS-Project\\UniversityMISv2\\src\\main\\resources\\university\\misv2\\universitymisv2\\student\\images\\College Students.png");
         Image UpdateImages=new Image(college.toURI().toString());
         studentsimage.setImage(UpdateImages);
-        user1.setStroke(Color.SEAGREEN);
-        Image im =new Image("C:\\Users\\iTEC\\Desktop\\Java\\JAVA-MIS-Project\\UniversityMISv2\\src\\main\\resources\\university\\misv2\\universitymisv2\\student\\images\\girl.png",false);
-        user1.setFill(new ImagePattern(im));
-        user1.setEffect( new DropShadow(+25d,0d,+2d,Color.DARKSEAGREEN));
+        File profilepic=new File("C:\\Users\\iTEC\\Desktop\\Java\\JAVA-MIS-Project\\UniversityMISv2\\src\\main\\resources\\university\\misv2\\universitymisv2\\student\\images\\Users.png");
+        Image UpdateImages2=new Image(profilepic.toURI().toString());
+        user1.setImage(UpdateImages2);
 
     }
 
